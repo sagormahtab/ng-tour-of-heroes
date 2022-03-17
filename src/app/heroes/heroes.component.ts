dc92fead0392de4b89d331b0dc02d84cec09eb0d
sagormahtab <sagormahtab@gmail.com>
2022-03-17 16:09:29 +0600

    explored list view

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { HEROES } from '../mock-heroes';
+import { Hero } from '../hero';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css'],
+})
+export class HeroesComponent implements OnInit {
+  heroes = HEROES;
+  selectedHero?: Hero;
+  onSelect(hero: Hero) {
+    this.selectedHero = hero;
+  }
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
