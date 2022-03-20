26af5e71a3303893434b15a8f0d2b05ee7636325
sagormahtab <sagormahtab@gmail.com>
2022-03-20 16:21:08 +0600

    dashboard component added

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -15,4 +15,10 @@ export class HeroService {
     this.messageService.add('Hero Service: fetched hereos');
     return heroes;
   }
+
+  getHero(id: number): Observable<Hero> {
+    const hero = HEROES.find((h) => h.id === id)!;
+    this.messageService.add(`HeroService: fatched hero id=${id}`);
+    return of(hero);
+  }
 }
